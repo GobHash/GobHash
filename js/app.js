fccd0daefc2fb14d1f1a527275d2ab4f6aa95fd2
Adolfo Morales <gadolfomoralesmartinez@gmail.com>
2017-10-02 19:38:16 -0600

    Add otherwise url redirect

--- js/app.js
+++ js/app.js
@@ -8,6 +8,8 @@
 
     config.$inject = ['$stateProvider', '$urlRouterProvider'];
     function config($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/");
+
         $stateProvider
             .state('home', {
                 url: '/',
