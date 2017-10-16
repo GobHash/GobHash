3f812a30a33ac8c637912d09a300b218ea4cffb9
Adolfo Morales <gadolfomoralesmartinez@gmail.com>
2017-10-15 20:52:38 -0600

    Add logout button

--- js/Controllers/Home/IndexController.js
+++ js/Controllers/Home/IndexController.js
@@ -5,8 +5,8 @@
         .module('gobhash')
         .controller('IndexController', IndexController);
 
-    IndexController.$inject = ['$http', '$state', '$scope'];
-    function IndexController($http, $state, $scope) {
+    IndexController.$inject = ['$http', '$state', '$scope', '$rootScope', 'AuthenticationService'];
+    function IndexController($http, $state, $scope, $rootScope, AuthenticationService) {
         var vm = this;
         // var token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFtb3JhbGVzIiwiaWQiOiI1OWRlYWJjYWI4OGU3MjAwMzhmNTM4OGMiLCJpYXQiOjE1MDc4NjExNzAsImV4cCI6MTUxMDQ1MzE3MH0.6Z9PxplofWz0okqgilrX7k4zjReqAqEymIGa8Au7Myc';
         // $http.defaults.headers.common['Authorization'] = token;
@@ -14,11 +14,17 @@
         $scope.UpdateHeader = UpdateHeader;
         vm.actualPath = '';
         vm.stateMapping = {}
+        vm.currentUser = $rootScope.globals.currentUser;
+        vm.Logout = Logout;
 
         $scope.UpdateHeader();
         function UpdateHeader() {
             vm.actualPath = $state.$current.name;
             vm.stateMapping.feed = (vm.actualPath === 'feed');
+        }
+
+        function Logout() {
+            AuthenticationService.Logout();
         }
     }
 
