00dbfc1a96e66f4cab7b16eb6ed45229c1554976
Adolfo Morales <gadolfomoralesmartinez@gmail.com>
2017-10-18 23:32:11 -0600

    Fix rege to accept emails with .gt

--- js/Controllers/User/RecoverController.js
+++ js/Controllers/User/RecoverController.js
@@ -17,7 +17,7 @@
 
         vm.recover = {};
         vm.RecoverPassword = RecoverPassword;
-        vm.emailRegex = /^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$/;
+        vm.emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
         function RecoverPassword() {
             vm.dataLoading = true;
